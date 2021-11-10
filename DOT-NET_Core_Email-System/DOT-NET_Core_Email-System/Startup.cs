88d7dfd296de17be481feed01f2dbef9fb446a7e
Ronak Padaliya <ronakpadaliya77@gmail.com>
2021-11-10 16:57:22 +0530

    compose added

--- DOT-NET_Core_Email-System/DOT-NET_Core_Email-System/Startup.cs
+++ DOT-NET_Core_Email-System/DOT-NET_Core_Email-System/Startup.cs
@@ -1,4 +1,4 @@
-using Email_System_ASPCORE.Models;
+using DOT_NET_Core_Email_System.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
