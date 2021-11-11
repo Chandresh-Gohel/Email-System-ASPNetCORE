0a39a0a029751c6b02d4cca641b8d622d1efc660
Chandresh Gohel <77381616+Chandresh-Gohel@users.noreply.github.com>
2021-11-12 03:29:05 +0530

    AllCurdOperationsDone

---
+++ DOT-NET_Core_Email-System/DOT-NET_Core_Email-System/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DOT_NET_Core_Email_System.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Key]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
